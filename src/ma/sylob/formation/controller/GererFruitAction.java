7f20e479cf9314ddf716e403a032a9a344ebe6ae
unknown <ka-maroc@MAROC34.sylob.local>
2017-11-20 15:56:01 +0000

    Use Servlet like Controller

--- src/ma/sylob/formation/controller/GererFruitAction.java
+++ src/ma/sylob/formation/controller/GererFruitAction.java
@@ -1,6 +1,8 @@
 package ma.sylob.formation.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,8 +27,9 @@ public class GererFruitAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = null;
+		rd = request.getRequestDispatcher("/index.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
